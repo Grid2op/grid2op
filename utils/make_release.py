075f88b15404428a87c1359e03196006f25c9058
DONNOT Benjamin <benjamin.donnot@rte-france.com>
2024-10-15 14:26:53 +0200

    ready for release version 1.10.5 [skip ci]
    Signed-off-by: DONNOT Benjamin <benjamin.donnot@rte-france.com>
    Signed-off-by: Xavier Weiss <xavierw@kth.se>

--- utils/make_release.py
+++ utils/make_release.py
@@ -197,10 +197,10 @@ if __name__ == "__main__":
             start_subprocess_print(["git", "add", f'{os.path.join(PATH_PREVIOUS_RUNNER, f"res_agent_{version}")}/*'])
 
         # Commit
-        start_subprocess_print(["git", "commit", "-s", "-S", "-m", "Release v{}".format(version)])
-        if not is_prerelease:
+        # start_subprocess_print(["git", "commit", "-s", "-S", "-m", "Release v{}".format(version)])
+        # if not is_prerelease:
             # Create a new git tag
-            start_subprocess_print(["git", "tag", "-s", "-a", "v{}".format(version), "-m", "Release v{}".format(version)])
+            # start_subprocess_print(["git", "tag", "-s", "-a", "v{}".format(version), "-m", "Release v{}".format(version)])
 
     if is_prerelease:
         print("Please push changes: 'git push'")
