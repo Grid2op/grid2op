f72adaecfa1e18ed2827f6b3282cb30d4ac0325e
Xavier Weiss <xavierw@kth.se>
2024-11-08 19:12:31 +0100

    Test: Update Obs Size and Str Contents
    Signed-off-by: Xavier Weiss <xavierw@kth.se>

--- grid2op/tests/test_issue_418.py
+++ grid2op/tests/test_issue_418.py
@@ -37,8 +37,8 @@ class Issue418Tester(unittest.TestCase):
             obs = gymenv.reset(seed=42)
             curt = np.array([1,1.,0.18852758,0.5537014,0.43770432,1])
             curt = np.array([-1,-1.,0.18852758,0.5537014,0.43770432,-1])
-            year = 571
-            day = 9
+            year = 229
+            day = 13
             # year = 1887
             # day = 9
         
