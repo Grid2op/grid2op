8584673a27f878484856fa40c5665cf83c41670f
Xavier Weiss <xavierw@kth.se>
2024-11-20 20:40:03 +0100

    Test: Fix package import in test_alert_trust_score
    Signed-off-by: Xavier Weiss <xavierw@kth.se>

--- grid2op/tests/test_alert_trust_score.py
+++ grid2op/tests/test_alert_trust_score.py
@@ -29,7 +29,7 @@ try: # Grid2OP available locally
                                             TestOpponentMultiLines,
                                             _get_blackout)
 except: # Grid2OP available as a package
-    from ._aux_opponent_for_test_alerts import (_get_steps_attack,
+    from _aux_opponent_for_test_alerts import (_get_steps_attack,
                                         TestOpponent,
                                         TestOpponentMultiLines,
                                         _get_blackout)
